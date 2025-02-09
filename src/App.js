54b49a8665f90a44fb691980abd0330eb036a5f9
Sandeep S <sandeepsatheesh.official@gmail.com>
2025-02-09 23:20:13 +0530

    Initial React app commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          More to come here soon!
+        </p>
+        Work In Progress by <a href='https://www.linkedin.com/in/sandeep-s-/'>Sandeep S</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
