cbca7f51d13260374050aa26c0ef243482f69705
Sandeep S <sandeepsatheesh.official@gmail.com>
2025-02-15 10:59:35 +0530

    feat(setup): Created logo ZRUI-1

---
+++ app/common/components/logo/logo.tsx
@@ -0,0 +1,12 @@
+import './logo.css';
+
+const Logo : React.FC = () => {
+    return (
+        <div className="logo-container">
+        <div className="halo"></div>
+        <div className="logo">Z</div>
+        </div>
+    );
+}
+
+export default Logo;
