b9cd4ee13a81650ed2f5f44a064c9e3a96a2b8dd
Sandeep S <sandeepsatheesh.official@gmail.com>
2025-02-22 14:00:23 +0530

    feat(setup): To load images in the right path ZRUI-1

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import './logo.css'
 import { useTheme } from '../../context/themecontext';
+
 interface LogoProps {
     className: string;
 }
@@ -15,11 +16,10 @@ const Logo: React.FC<LogoProps> = ({ className }) => {
   
     if (!isClient) return null; // Prevents hydration mismatch
 
-    console.log(import.meta.env);
-    var logoPath = `${import.meta.env.BASE_URL}/assets/zero2react-dark.svg`;
+    var logoPath = `dist/assets/zero2react-dark.svg`;
 
     if (theme === 'light') {
-        logoPath = `${import.meta.env.BASE_URL}/assets/zero2react-light.svg`;
+        logoPath = `dist/assets/zero2react-light.svg`;
     }
     return (
         <main className={className + " logo-container"}>
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-const isGitHubPages = process.env.NODE_ENV === 'production';
 console.log(process.env);
+const isGitHubPages = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   plugins: [react()],
