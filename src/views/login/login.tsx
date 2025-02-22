d4f6ce1c8a1b5aa886bbc55e6bd6053b75b14566
Sandeep S <sandeepsatheesh.official@gmail.com>
2025-02-23 00:17:44 +0530

    feat(setup): Added login screen skeleton, refactored CSS and other minor stuff ZRUI-1

--- src/views/login/login.tsx
+++ src/views/login/login.tsx
@@ -1,20 +1,73 @@
 import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
+import { useForm } from "react-hook-form";
+import './login.css';
+import { useTheme } from '../../context/themecontext';
+
 const Login: React.FC = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const getGoogleSignInTheme = () => {
+        return useTheme().theme === 'dark' ? 'filled_black' : 'outline';
+    }
+
+
     return (
-        <main className='d-flex mt-5 flex-column justify-content-center align-items-center'>
-            <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_SIGNIN_CLIENT_ID}>
-                <h1 className="text-7xl mt-4 font-bold text-center tagline">Get Started</h1>
-                <GoogleLogin
-                    onSuccess={credentialResponse => {
-                        console.log(credentialResponse);
-                    }}
-                    onError={() => {
-                        console.log('Login Failed');
-                    }}
-                    useOneTap
-                />
-            </GoogleOAuthProvider>
-        </main>
+        <div className="d-flex h-100 w-100 justify-content-center align-items-center flex-column">
+            <h2 className="text-center mb-5">Sign in to Your Investments Dashboard</h2>
+            <div className="card p-4 shadow-lg" style={{ width: "22rem" }}>
+                <h2 className="text-center mb-4">Login</h2>
+                <form>
+                    <div className="mb-3">
+                        <label className="form-label">Email:</label>
+                        <input
+                            type="email"
+                            className="form-control login-form-control"
+                            {...register("email", { required: "Email is required" })}
+                            placeholder="Enter your email"
+                        />
+                        {errors.email && <div className="text-danger">Invalid Email!</div>}
+                    </div>
+
+                    {/* Password Field */}
+                    <div className="mb-3">
+                        <label className="form-label">Password:</label>
+                        <input
+                            type="password"
+                            className="form-control login-form-control"
+                            {...register("password", { required: "Password is required" })}
+                            placeholder="Enter your password"
+                        />
+                        {errors.password && <div className="text-danger">Invalid Password!</div>}
+                    </div>
+
+                    {/* Submit Button */}
+                    <button type="submit" className="login-form-control login-button w-100">
+                        Login
+                    </button>
+                </form>
+                <div className='w-100 d-flex flex-column justify-content-center align-items-center'>
+                    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_SIGNIN_CLIENT_ID}>
+                        <h5 className="text-7xl mt-2 mb-2 font-weight-bold text-center">Or</h5>
+                        <div className='google-login-button'>
+                            <GoogleLogin
+                                theme={getGoogleSignInTheme()} // Dark mode button
+                                onSuccess={credentialResponse => {
+                                    console.log(credentialResponse);
+                                }}
+                                onError={() => {
+                                    console.log('Login Failed');
+                                }}
+                                useOneTap
+                            />
+                        </div>
+                    </GoogleOAuthProvider>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- src/views/welcome/welcome.tsx
+++ src/views/welcome/welcome.tsx
@@ -5,14 +5,14 @@ import { useNavigate } from "react-router-dom";
 const Welcome: React.FC = () => {
     const navigate = useNavigate();
     return (
-        <div className='d-flex h-100 flex-column justify-content-center align-items-center'>
-            <main className='p-5 items-center justify-center'>
-                <Logo />
+        <div className='h-100 d-flex justify-content-center align-items-center'>
+            <main className='d-flex flex-column align-items-center justify-center'>
+                <Logo className='pt-3 flex-1 h-100 w-100' />
                 <h1 className="text-7xl mt-4 font-bold text-center tagline">Invest. Grow. Succeed.</h1>
                 <button type="button"
                     className="text-4xl mt-4 pt-3  btn w-100 btn-rounded btn-lg btn-get-started"
                     onClick={() => navigate('/login')}>
-                        <h2 className='font-weight-bold'>Get Started</h2>
+                        <h3 className='font-weight-bold'>Get Started</h3>
                 </button>
             </main>
         </div>
